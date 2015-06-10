43343793d92ce3efc708d3b8d7edcdc9c0ca4244
Cody Smith <cody@localhost.localdomain>
2015-06-10 18:45:35 -0500

    Updated add/removes

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-Test ADD. Delete When done.
+# TO-DO
+# - Clean up initial echo line. 
+# - Make organized (alphabetical) reason list of why you choose to install/remove
+#   certain packages.
 
+
+echo "Make sure the following repos are installed first.\n"
+echo "RPM Fusion Free\n"
+echo "RPM Fusion Non-Free\n"
+echo "Adobe's Shitty Nonsense.\n\n\n"
+
+dnf remove \
+	transmission.*
+	orca.*
+
+dnf install \
+	deluge*
+	cherrytree
+	ALL THE DAMN GSTREAMER PLUGINS
+	vlc
+	steam
+	flash-plugin
+	homebank
+	
